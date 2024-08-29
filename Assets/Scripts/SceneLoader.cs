6d218dac0ff2cea1afc095f1369e4798ba0a6a5e
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-08-29 16:02:33 -0500

    feat: include new assets into tutorial and quests interfaces

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] private string sceneToLoad;
 
     public void LoadScene(string sceneName)
     {
