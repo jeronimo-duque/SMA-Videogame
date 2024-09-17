fc7862ccef0f0ad8f8cd9569e91f3c34496bcb13
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2024-09-16 23:26:29 -0500

    feat: include multiple-choice quesitons mechanic
    Has been used UnityEvents to handle the user navigation during the implementation.

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    [Header("Possible options")]
+    [SerializeField] private List<TextMeshProUGUI> answerOptions;
+
+    [Header("Correct option")]
+    [SerializeField] private TextMeshProUGUI correctAnswer;
+
+    [Space(10)]
+    [Header("Positive feedback")]
+    public TriviaGameOverEvent answerCorrectlyEvent;
+    [SerializeField] private string positiveFeedbackTitle;
+    [SerializeField] private string positiveFeedbackMessage;
+    [SerializeField] private string positiveSuggestionTitle;
+
+    [Header("Negative feedback")]
+    public TriviaGameOverEvent gameOverEvent;
+    [SerializeField] private string negativeFeedbackTitle;
+    [SerializeField] private string negativeFeedbackMessage;
+    [SerializeField] private string negativeSuggestionTitle;
+
+    public void ValidateAnswer(TextMeshProUGUI selectedOption)
+    {
+        
+        if (selectedOption.text != correctAnswer.text)
+        {
+            // Invoke the game over event with 3 args.
+            gameOverEvent.Invoke(negativeFeedbackTitle, negativeFeedbackMessage, negativeSuggestionTitle);
+            return;
+        }
+
+        answerCorrectlyEvent.Invoke(positiveFeedbackTitle, positiveFeedbackMessage,positiveSuggestionTitle);
+
+
+    }
+}
---
+++ Assets/Scripts/TriviaManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class TriviaGameOverEvent : UnityEvent<string, string,string> { }
+// First param -> Title feedback message
+// Second param -> Feedback message
+// Third param -> Suggestion feedback message
+public class TriviaManager : MonoBehaviour
+{
+
+    [Header("Navigation between questions")]
+    [SerializeField] private List<GameObject> questions;
+    [SerializeField] private int currentQuestion = 0;
+
+
+    [Space(10)]
+    [Header("Feedback UI")]
+    [SerializeField] private GameObject positiveFeedbackUI;
+    [SerializeField] private TextMeshProUGUI positiveFeedbackTitle;
+    [SerializeField] private TextMeshProUGUI positiveFeedbackMessage;
+    [SerializeField] private TextMeshProUGUI positiveSuggestionMessage;
+
+    [Header("Negative feedback")]
+    [SerializeField] private GameObject negativeFeedbackUI;
+    [SerializeField] private TextMeshProUGUI negativeFeedbackTitle;
+    [SerializeField] private TextMeshProUGUI negativeFeedbackMessage;
+    [SerializeField] private TextMeshProUGUI negativeSuggestionMessage;
+
+    [Header("Win feedback")]
+    [SerializeField] private GameObject winFeedbackUI;
+
+
+    private void Awake()
+    {
+        Debug.Log(questions.Count);
+    }
+    public void ShowNegativeFeedback(string title, string message, string suggestion)
+    {
+        Debug.Log("Bad");
+        // Put the information in each TMPro assigned.
+        negativeFeedbackUI.SetActive(true);
+        negativeFeedbackTitle.text = title;
+        negativeFeedbackMessage.text = message;
+        negativeSuggestionMessage.text = suggestion;
+
+        // Hide the question interface
+        questions[currentQuestion].SetActive(false);
+    }
+
+    public void ShowPositiveFeedback(string title, string message, string suggestion)
+    {
+        Debug.Log("Nice");
+        // Put the information in each TMPro assigned.
+        positiveFeedbackUI.SetActive(true);
+        positiveFeedbackTitle.text = title;
+        positiveFeedbackMessage.text = message;
+        positiveSuggestionMessage.text = suggestion;
+
+        // Hide the question interface
+        questions[currentQuestion].SetActive(false);
+
+    }
+
+    public void RetryQuestion()
+    {
+        negativeFeedbackUI.SetActive(false);
+
+        // Show again the current question.
+        questions[currentQuestion].SetActive(true);
+    }
+
+    public void ShowNextQuestion()
+    {
+
+        // If the index hasn't exceeded the maximum list length, execute the logic for the next question.
+        if (currentQuestion < questions.Count)
+        {
+            // Hide the current question
+            questions[currentQuestion].SetActive(false);
+            currentQuestion++;
+  
+            // If the user has answered all the questions, don't show anymore.
+            if(currentQuestion == questions.Count)
+            {
+                Debug.Log("GG");
+                winFeedbackUI.SetActive(true);
+                return;
+            }
+            
+            // Active the next question and hide the positive feedback.
+            questions[currentQuestion].SetActive(true);
+            positiveFeedbackUI.SetActive(false);
+        }
+    }
+}
